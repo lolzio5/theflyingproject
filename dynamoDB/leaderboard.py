936a0ba1f801e095c7d7c69c485c0b1f3bca1bf1
rrroooyyywang <rrroooyyywang@outlook.com>
2024-03-12 15:10:39 +0000

    support load data to dynamoDB

--- dynamoDB/leaderboard.py
+++ dynamoDB/leaderboard.py
@@ -9,7 +9,7 @@ def create_leaderboard_table(dynamodb=None):
         KeySchema=[
             {
                 'AttributeName': 'UserID',
-                'KeyType': 'HASH'  # Primary key
+                'KeyType': 'S'  # Primary key
             },
         ],
         AttributeDefinitions=[
@@ -17,6 +17,18 @@ def create_leaderboard_table(dynamodb=None):
                 'AttributeName': 'PlayedTime',
                 'AttributeType': 'S'
             },
+            {
+                'AttributeName': 'TimeAtTriggerBox1',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'TimeAtTriggerBox2',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'TimeAtTriggerBox3',
+                'AttributeType': 'S'
+            }
         ],
         ProvisionedThroughput={
             'ReadCapacityUnits': 10,
@@ -24,6 +36,39 @@ def create_leaderboard_table(dynamodb=None):
         }
     )
     return table
+
+def update_leaderboard(dynamodb, json_data):
+    is_leaderboard_message = 0
+    
+    leaderboard_dict = json_data.load()
+    if leaderboard_dict.get("leaderboard"):
+        player = leaderboard_table.get("player")
+        is_leaderboard_message = 1
+        
+        if dynamodb:
+            table = dynamodb.Table('Leaderboard')
+            
+            table.update_item(
+                            key={
+                                'UserID':player,
+                            },
+                            UpdateExpression='SET PlayedTime=:val1',
+                            ExpressionAttributeValues={
+                                ':val1': leaderboard_dict.get("PlayedTime")
+                            }
+                        )
+            for i in range(3):
+                    if leaderboard_dict.get("TimeAtTriggerBox" + str(i)):
+                        table.update_item(
+                            key={
+                                'UserID':player,
+                            },
+                            UpdateExpression='SET TimeAtTriggerBox'+str(i)+'= :val1',
+                            ExpressionAttributeValues={
+                                ':val1': leaderboard_dict.get("TimeAtTriggerBox"+str(i))
+                            }
+                        )
+    return is_leaderboard_message
 
 if __name__ == '__main__':
     leaderboard_table = create_leaderboard_table()
